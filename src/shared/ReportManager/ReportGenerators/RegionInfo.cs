5ea1f47bfcdc1e4c7db428b3ba3288c29769e539
Xiangzhi Sheng <xisheng@microsoft.com>
2019-10-15 15:39:41 +0800

    Remove unnecessary blob reference (#2)

--- src/shared/ReportManager/ReportGenerators/RegionInfo.cs
+++ src/shared/ReportManager/ReportGenerators/RegionInfo.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 
 namespace Microsoft.Azure.CCME.Assessment.Managers.ReportGenerators
 {
     class RegionInfo
     {
-        public bool IsChinaRegion { get; set; }
-
         public CultureInfo CurrencyCulture { get; set; }
 
         public string CurrencySymbol { get; set; }
@@ -18,7 +11,5 @@ namespace Microsoft.Azure.CCME.Assessment.Managers.ReportGenerators
         public string CurrencyUnit { get; set; }
 
         public string TargetRegionName { get; set; }
-
-        public List<string> TargetResoureTypes { get; set; }
     }
 }
